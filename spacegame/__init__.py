f7b7e7aa185068052a9e0ebe4d8c7e8d4f8de2da
Evan Davis <me@ezdwt.com>
2016-08-16 18:41:31 -0700

    Moved logging handlers to their own module.

--- spacegame/__init__.py
+++ spacegame/__init__.py
@@ -1,12 +1,4 @@
-import logging
-from pantsmud.driver import hook
-
-def open_brain_hook(_, brain):
-    logging.debug("brain %r opened" % brain)
-
-def close_brain_hook(_, brain):
-    logging.debug("brain %r closed" % brain)
+from spacegame import handler
 
 def init():
-    hook.add(hook.HOOK_OPEN_BRAIN, open_brain_hook)
-    hook.add(hook.HOOK_CLOSE_BRAIN, close_brain_hook)
+    handler.init()

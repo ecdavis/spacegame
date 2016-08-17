3873ad7b7631cc17a365ca382b4f8e82e59de088
Evan Davis <me@ezdwt.com>
2016-08-16 19:47:35 -0700

    Integrated the universe into the startup script.

--- start_spacegame.py
+++ start_spacegame.py
@@ -1,7 +1,14 @@
 import logging
+import os.path
 import pants
 from pantsmud.driver import game, net
 import spacegame
+from spacegame.universe import persist
+
+
+UNIVERSE_PATH = os.path.abspath("data/universe/")
+UNIVERSE_FILE = os.path.join(UNIVERSE_PATH, "universe.json")
+
 
 if __name__ == '__main__':
     if __debug__:
@@ -11,7 +18,8 @@ if __name__ == '__main__':
 
     # Initialize the game.
     engine = pants.Engine.instance()
-    game.init(engine, None)
+    universe = persist.load_universe(UNIVERSE_FILE)
+    game.init(engine, universe)
     spacegame.init()
     net.init()
     game.start()

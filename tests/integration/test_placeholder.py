e005703171bdc5a2016a574bbc279b24358b56c2
Evan Davis <me@ezdwt.com>
2016-09-09 20:58:44 -0700

    #16: Added integration tests for the login module.

--- tests/integration/test_placeholder.py
+++
@@ -1,6 +0,0 @@
-from tests.integration.util import IntegrationTestCase
-
-
-class PlaceholderIntegrationTestCase(IntegrationTestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)

3c771222d45a1df840454d2250f5ea2c37785385
Evan Davis <me@ezdwt.com>
2016-10-10 22:34:41 -0700

    #46: Add getter and setter unit tests. (#72)
    * Add getter and setter unit tests.
    
    * Add EntityUnitTestCase to unit test suite.
    
    * Add missing comma.

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,12 +1,18 @@
 import unittest
 from tests.unit.test_application import ApplicationUnitTestCase
+from tests.unit.test_celestial import CelestialUnitTestCase
+from tests.unit.test_entity import EntityUnitTestCase
 from tests.unit.test_config import PathConfigUnitTestCase
+from tests.unit.test_mobile import MobileUnitTestCase
 from tests.unit.test_universe import UniverseUnitTestCase
 
 
 def get_unit_tests():
     return unittest.TestSuite([
         unittest.TestLoader().loadTestsFromTestCase(ApplicationUnitTestCase),
+        unittest.TestLoader().loadTestsFromTestCase(CelestialUnitTestCase),
+        unittest.TestLoader().loadTestsFromTestCase(EntityUnitTestCase),
+        unittest.TestLoader().loadTestsFromTestCase(MobileUnitTestCase),
         unittest.TestLoader().loadTestsFromTestCase(PathConfigUnitTestCase),
         unittest.TestLoader().loadTestsFromTestCase(UniverseUnitTestCase)
     ])

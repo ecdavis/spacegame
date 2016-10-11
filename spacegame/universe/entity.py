0d1ce6d898f97910ffd1fa6d17d485004232b563
Evan Davis <me@ezdwt.com>
2016-10-10 23:33:40 -0700

    #56: Remove Mobile class. (#75)

--- spacegame/universe/entity.py
+++ spacegame/universe/entity.py
@@ -11,6 +11,7 @@ class Entity(object):
         self.uuid = uuid.uuid4()
         self.name = ""
         self.universe = None
+        self.brain_uuid = None
         self.celestial_uuid = None
         self.position = (0, 0, 0)
         self.vector = (1.0, 0.0, 0.0)
@@ -52,6 +53,56 @@ class Entity(object):
         }
 
     @property
+    def environment(self):
+        """
+        Get the Entity's Environment, if it has one.
+        """
+        # TODO This is really hacky. Either rename Universe to World or reconsider PantsMUD's
+        # TODO on brains having a world attached.
+        return self.universe
+
+    @property
+    def brain(self):
+        """
+        Get the Entity's Brain, if it has one.
+        """
+        if self.brain_uuid:
+            return self.universe.brains[self.brain_uuid]
+        else:
+            return self.brain_uuid
+
+    @brain.setter
+    def brain(self, brain):
+        """
+        Set the Entity's Brain.
+        """
+        if brain:
+            self.brain_uuid = brain.uuid
+        else:
+            self.brain_uuid = None
+
+    def attach_brain(self, brain):
+        """
+        Attach a Brain to this Entity.
+        """
+        self.brain = brain
+        brain.mobile = self
+
+    def detach_brain(self):
+        """
+        Detach a Brain from this Entity.
+        """
+        self.brain.mobile = None
+        self.brain = None
+
+    def message(self, name, data=None):
+        """
+        Send a message to the Entity's Brain, if it has one.
+        """
+        if self.brain:
+            self.brain.message(name, data)
+
+    @property
     def celestial(self):
         """
         Get the Entity's Celestial, if it has one.
@@ -90,3 +141,7 @@ class Entity(object):
         return (round(self.vector[0]*self.speed, 3),  # X
                 round(self.vector[1]*self.speed, 3),  # Y
                 round(self.vector[2]*self.speed, 3))  # Z
+
+
+def new_mobile():
+    return Entity()

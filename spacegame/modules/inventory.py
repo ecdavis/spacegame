41abd9dfe6f73caef3aa8426365916d0927d8e39
Evan Davis <me@ezdwt.com>
2016-10-11 22:07:28 -0700

    #54: Add support for one fitted item per entity. (#78)
    * Add test command to add an item to the player's inventory.
    
    * Add fitted item slot to inventory aux data.
    
    * Add fit and unfit commands to change fitted item.

--- spacegame/modules/inventory.py
+++ spacegame/modules/inventory.py
@@ -1,15 +1,21 @@
 import pantsmud
 from pantsmud.driver import auxiliary, command, hook, parser
-from pantsmud.util import message
+from pantsmud.util import error, message
 from spacegame.core import aux_types, hook_types
 from spacegame.universe import item
 
 
 class InventoryAux(object):
     def __init__(self):
+        self.fitted = None
         self.inventory = []
 
     def load_data(self, data):
+        if data["fitted"]:
+            fitted_item = item.Item()
+            fitted_item.load_data(data["fitted"])
+            pantsmud.game.environment.add_item(fitted_item)
+            self.fitted = fitted_item
         inv_items = []
         for item_data in data["inventory"]:
             inv_item = item.Item()
@@ -20,6 +26,7 @@ class InventoryAux(object):
 
     def save_data(self):
         return {
+            "fitted": self.fitted.save_data() if self.fitted else None,
             "inventory": [inv_item.save_data() for inv_item in self.inventory]
         }
 
@@ -27,12 +34,58 @@ class InventoryAux(object):
 def inventory_command(brain, cmd, args):
     parser.parse([], args)
     mobile = brain.mobile
+    fitted_data = mobile.aux["inventory"].fitted
+    if fitted_data:
+        fitted_data = {fitted_data.name: str(fitted_data.uuid)}
     inventory = mobile.aux["inventory"].inventory
     inventory_data = {i.name: str(i.uuid) for i in inventory}
-    message.command_success(mobile, cmd, {"inventory": inventory_data})
+    message.command_success(mobile, cmd, {"fitted": fitted_data, "inventory": inventory_data})
+
+
+def fit_command(brain, cmd, args):
+    params = parser.parse([("item_uuid", parser.UUID)], args)
+    mobile = brain.mobile
+    if mobile.aux["inventory"].fitted:
+        raise error.CommandFail()
+    inventory = mobile.aux["inventory"].inventory[:]
+    fittable_item = None
+    for i in inventory:
+        if i.uuid == params["item_uuid"]:
+            fittable_item = i
+    if fittable_item is None:
+        raise error.CommandFail()
+    inventory = [i for i in inventory if i.uuid != params["item_uuid"]]
+    mobile.aux["inventory"].fitted = fittable_item
+    mobile.aux["inventory"].inventory = inventory
+    message.command_success(mobile, cmd)
+
+
+def unfit_command(brain, cmd, args):
+    parser.parse([], args)
+    mobile = brain.mobile
+    if mobile.aux["inventory"].fitted is None:
+        raise error.CommandFail()
+    inventory = mobile.aux["inventory"].inventory[:]
+    fitted_item = mobile.aux["inventory"].fitted
+    inventory.append(fitted_item)
+    mobile.aux["inventory"].fitted = None
+    mobile.aux["inventory"].inventory = inventory
+    message.command_success(mobile, cmd)
+
+
+def test_add_active_warp_scanner_command(brain, cmd, args):
+    parser.parse([], args)
+    mobile = brain.mobile
+    i = item.Item()
+    i.name = "Active Warp Scanner"
+    mobile.aux["inventory"].inventory.append(i)
+    pantsmud.game.environment.add_item(i)
+    message.command_success(mobile, cmd, {"item": str(i.uuid)})
 
 
 def clear_inventory_hook(_, mobile):
+    if mobile.aux["inventory"].fitted is not None:
+        pantsmud.game.environment.remove_item(mobile.aux["inventory"].fitted)
     for i in mobile.aux["inventory"].inventory:
         pantsmud.game.environment.remove_item(i)
 
@@ -40,4 +93,7 @@ def clear_inventory_hook(_, mobile):
 def init():
     auxiliary.install(aux_types.AUX_TYPE_ENTITY, "inventory", InventoryAux)
     command.add_command("inventory", inventory_command)
+    command.add_command("fit", fit_command)
+    command.add_command("unfit", unfit_command)
+    command.add_command("test.add_active_warp_scanner", test_add_active_warp_scanner_command)
     hook.add(hook_types.REMOVE_MOBILE, clear_inventory_hook)
--- tests/integration/test_inventory.py
+++ tests/integration/test_inventory.py
@@ -3,14 +3,29 @@ from tests.integration.util import IntegrationTestCase
 
 
 class InventoryIntegrationTestCase(IntegrationTestCase):
-    def test_inventory(self):
+    def test_inventory_empty(self):
         client = self.get_client()
-        self.register_and_login(client, "test_inventory")
+        self.register_and_login(client, "test_inventory_empty")
         client.send("inventory\r\n")
         response = json.loads(client.recv(4096))
         self.assertEqual("command.success", response["message"])
         self.assertEqual("inventory", response["data"]["command"])
+        self.assertIsNone(response["data"]["result"]["fitted"])
         self.assertEqual({}, response["data"]["result"]["inventory"])
+
+    def test_inventory_one_item(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_inventory_one_item")
+        client.send("test.add_active_warp_scanner\r\n")
+        response1 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response1["message"])
+        self.assertEqual("test.add_active_warp_scanner", response1["data"]["command"])
+        client.send("inventory\r\n")
+        response2 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response2["message"])
+        self.assertEqual("inventory", response2["data"]["command"])
+        self.assertIsNone(response2["data"]["result"]["fitted"])
+        self.assertEqual(1, len(response2["data"]["result"]["inventory"]))
 
     def test_inventory_with_parameters_returns_error(self):
         client = self.get_client()
@@ -19,4 +34,95 @@ class InventoryIntegrationTestCase(IntegrationTestCase):
         response = json.loads(client.recv(4096))
         self.assertEqual("command.error", response["message"])
         self.assertEqual("inventory", response["data"]["command"])
+        # TODO Verify error message
+
+    def test_fit(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_fit")
+        client.send("test.add_active_warp_scanner\r\n")
+        response1 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response1["message"])
+        self.assertEqual("test.add_active_warp_scanner", response1["data"]["command"])
+        fittable_item_uuid = response1["data"]["result"]["item"]
+        client.send("fit %s\r\n" % fittable_item_uuid)
+        response2 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response2["message"])
+        self.assertEqual("fit", response2["data"]["command"])
+
+    def test_fit_with_already_fitted_item_returns_failure(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_fit_with_already_fitted_item")
+        client.send("test.add_active_warp_scanner\r\n")
+        response1 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response1["message"])
+        self.assertEqual("test.add_active_warp_scanner", response1["data"]["command"])
+        fittable_item1_uuid = response1["data"]["result"]["item"]
+        client.send("fit %s\r\n" % fittable_item1_uuid)
+        response2 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response2["message"])
+        self.assertEqual("fit", response2["data"]["command"])
+
+        client.send("test.add_active_warp_scanner\r\n")
+        response3 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response3["message"])
+        self.assertEqual("test.add_active_warp_scanner", response3["data"]["command"])
+        fittable_item2_uuid = response1["data"]["result"]["item"]
+        client.send("fit %s\r\n" % fittable_item2_uuid)
+        response4 = json.loads(client.recv(4096))
+        self.assertEqual("command.fail", response4["message"])
+        self.assertEqual("fit", response4["data"]["command"])
+        # TODO Verify error message
+
+    def test_fit_with_non_existent_item_returns_failure(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_fit_with_non_existent_item")
+        client.send("fit 00000000-0000-0000-0000-000000000000\r\n")
+        response = json.loads(client.recv(4096))
+        self.assertEqual("command.fail", response["message"])
+        self.assertEqual("fit", response["data"]["command"])
+        # TODO Verify error message
+
+    def test_fit_with_no_parameters_returns_error(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_fit_with_no_parameters")
+        client.send("fit\r\n")
+        response = json.loads(client.recv(4096))
+        self.assertEqual("command.error", response["message"])
+        self.assertEqual("fit", response["data"]["command"])
+        # TODO Verify error message
+
+    def test_unfit(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_unfit")
+        client.send("test.add_active_warp_scanner\r\n")
+        response1 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response1["message"])
+        self.assertEqual("test.add_active_warp_scanner", response1["data"]["command"])
+        fittable_item_uuid = response1["data"]["result"]["item"]
+        client.send("fit %s\r\n" % fittable_item_uuid)
+        response2 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response2["message"])
+        self.assertEqual("fit", response2["data"]["command"])
+
+        client.send("unfit\r\n")
+        response3 = json.loads(client.recv(4096))
+        self.assertEqual("command.success", response3["message"])
+        self.assertEqual("unfit", response3["data"]["command"])
+
+    def test_unfit_with_no_fitted_item_returns_failure(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_unfit_with_no_fitted_item")
+        client.send("unfit\r\n")
+        response = json.loads(client.recv(4096))
+        self.assertEqual("command.fail", response["message"])
+        self.assertEqual("unfit", response["data"]["command"])
+        # TODO Verify error message
+
+    def test_unfit_with_parameters_returns_error(self):
+        client = self.get_client()
+        self.register_and_login(client, "test_unfit_with_parameters")
+        client.send("unfit one\r\n")
+        response = json.loads(client.recv(4096))
+        self.assertEqual("command.error", response["message"])
+        self.assertEqual("unfit", response["data"]["command"])
         # TODO Verify error message

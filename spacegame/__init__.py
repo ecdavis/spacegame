01d0cb0412011cebe9419e9e28041806cd24449c
Evan Davis <me@ezdwt.com>
2017-03-11 14:48:47 -0800

    pass init dependencies to spacegame package init

--- spacegame/__init__.py
+++ spacegame/__init__.py
@@ -1,14 +1,12 @@
-from pantsmud.driver import auxiliary, command, hook
 import spacegame.core
-from spacegame.core import login_manager
 import spacegame.commands
 import spacegame.modules
 
 
-def init():
-    spacegame.core.init(hook)
-    spacegame.modules.init(auxiliary, command, hook, login_manager)  # Init modules after core
-    spacegame.commands.init(command)  # Init commands after modules
+def init(auxiliaries, game_commands, hooks, login_commands):
+    spacegame.core.init(hooks)
+    spacegame.modules.init(auxiliaries, game_commands, hooks, login_commands)  # Init modules after core
+    spacegame.commands.init(game_commands)  # Init commands after modules
 
 
 def start():

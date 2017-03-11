e2df876599f915b88fa6a2eca8f5da84db32eec5
Evan Davis <me@ezdwt.com>
2017-03-11 15:49:56 -0800

    refactor echo module to use service/endpoint design

--- spacegame/modules/__init__.py
+++ spacegame/modules/__init__.py
@@ -3,7 +3,7 @@ from spacegame.modules import chat, echo, info, inventory, jump, login, thrust, 
 
 def init(auxiliaries, game_commands, hooks, login_commands, universe):
     chat.init(game_commands, universe)
-    echo.init(game_commands)
+    echo.init(game_commands, universe)
     info.init(game_commands)
     inventory.init(auxiliaries, game_commands, hooks)
     jump.init(game_commands)
--- spacegame/modules/echo.py
+++ spacegame/modules/echo.py
@@ -1,32 +1,81 @@
-import pantsmud.game
 from pantsmud.driver import hook, parser
 from pantsmud.util import message
 from spacegame.core import user
 
 
-def echo_command(brain, cmd, args):
-    mobile = brain.mobile
-    message.command_success(mobile, cmd, {"line": args})
+class Service(object):
+    def __init__(self, hooks, messages, universe, users):
+        self.hooks = hooks
+        self.messages = messages
+        self.universe = universe
+        self.users = users
+
+    def echo(self, mobile, line):
+        self.messages.command_success(mobile, "echo", {"line": line})
+
+    def quit(self, brain):
+        if brain.mobile and brain.identity:
+            self.users.save_player(brain.mobile)
+        brain.close()
+
+    def shutdown(self):
+        for m in self.universe.get_mobiles():
+            if m.brain.identity:
+                self.users.save_player(m)
+                m.brain.close()
+        self.hooks.run(self.hooks.HOOK_SHUTDOWN)
 
 
-def quit_command(brain, _, args):
-    parser.parse([], args)
-    if brain.mobile and brain.identity:
-        user.save_player(brain.mobile)
-    brain.close()
+class Endpoint(object):
+    def __init__(self, service):
+        self.service = service
+
+    def echo(self, request):
+        self.service.echo(
+            request["mobile"],
+            request["line"]
+        )
+
+    def quit(self, request):
+        self.service.quit(
+            request["brain"]
+        )
+
+    def shutdown(self, request):
+        self.service.shutdown()
 
 
-def shutdown_command(brain, _, args):
-    parser.parse([], args)
-    universe = pantsmud.game.environment
-    for m in universe.get_mobiles():
-        if m.brain.identity:
-            user.save_player(m)
-            m.brain.close()
-    hook.run(hook.HOOK_SHUTDOWN)
+def make_echo_command(endpoint):
+    def echo_command(brain, cmd, args):
+        request = {
+            "mobile": brain.mobile,
+            "line": args
+        }
+        endpoint.echo(request)
+    return echo_command
 
 
-def init(commands):
-    commands.add_command("echo", echo_command)
-    commands.add_command("quit", quit_command)
-    commands.add_command("shutdown", shutdown_command)
+def make_quit_command(endpoint):
+    def quit_command(brain, cmd, args):
+        parser.parse([], args)
+        request = {
+            "brain": brain
+        }
+        endpoint.quit(request)
+    return quit_command
+
+
+def make_shutdown_command(endpoint):
+    def shutdown_command(brain, _, args):
+        parser.parse([], args)
+        request = {}
+        endpoint.shutdown(request)
+    return shutdown_command
+
+
+def init(commands, universe):
+    service = Service(hook, message, universe, user)
+    endpoint = Endpoint(service)
+    commands.add_command("echo", make_echo_command(endpoint))
+    commands.add_command("quit", make_quit_command(endpoint))
+    commands.add_command("shutdown", make_shutdown_command(endpoint))

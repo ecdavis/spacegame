8b6946c91f088d2ccd49b5e9810c503442a44d85
Evan Davis <me@ezdwt.com>
2017-03-11 16:12:49 -0800

    refactor inventory module to use service/endpoint design

--- spacegame/modules/__init__.py
+++ spacegame/modules/__init__.py
@@ -5,7 +5,7 @@ def init(auxiliaries, game_commands, hooks, login_commands, universe):
     chat.init(game_commands, universe)
     echo.init(game_commands, universe)
     info.init(game_commands)
-    inventory.init(auxiliaries, game_commands, hooks)
+    inventory.init(auxiliaries, game_commands, hooks, universe)
     jump.init(game_commands)
     login.init(login_commands)
     thrust.init(game_commands)
